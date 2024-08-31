cedf0dd2f015afbaf4b9e25dc563c5530f07f041
Rohan <149697636+rohan-de@users.noreply.github.com>
2024-08-31 22:33:24 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const items = document.querySelectorAll('.my-detail');
+  const items = document.querySelectorAll('.detail-container');
 
   function checkVisibility() {
     const windowHeight = window.innerHeight;
@@ -20,28 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
   checkVisibility();
 });
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  const items = document.querySelectorAll('.my-services');
-
-  function checkVisibility() {
-    const windowHeight = window.innerHeight;
-    items.forEach(item => {
-      const rect = item.getBoundingClientRect();
-      if (rect.top <= windowHeight && rect.bottom >= 0) {
-        item.classList.add('visible');
-      } else {
-        item.classList.remove('visible');
-      }
-    });
-  }
-
-  window.addEventListener('scroll', checkVisibility);
-  window.addEventListener('resize', checkVisibility);
-
-  // Initial check in case items are already in view
-  checkVisibility();
-});
 
 document.addEventListener('DOMContentLoaded', () => {
   const items = document.querySelectorAll('.contents');
