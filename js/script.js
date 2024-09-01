8a082558410ffe580690e2bde1e3f4c0a4aec66f
Rohan <149697636+rohan-de@users.noreply.github.com>
2024-09-01 10:46:54 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -42,8 +42,55 @@ document.addEventListener('DOMContentLoaded', () => {
   // Initial check in case items are already in view
   checkVisibility();
 });
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const items = document.querySelectorAll('.sr-vc');
+
+  function checkVisibility() {
+    const windowHeight = window.innerHeight;
+    items.forEach(item => {
+      const rect = item.getBoundingClientRect();
+      if (rect.top <= windowHeight && rect.bottom >= 0) {
+        item.classList.add('visible');
+      } else {
+        item.classList.remove('visible');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', checkVisibility);
+  window.addEventListener('resize', checkVisibility);
+
+  // Initial check in case items are already in view
+  checkVisibility();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const items = document.querySelectorAll('.hero-first-col');
+
+  function checkVisibility() {
+    const windowHeight = window.innerHeight;
+    items.forEach(item => {
+      const rect = item.getBoundingClientRect();
+      if (rect.top <= windowHeight && rect.bottom >= 0) {
+        item.classList.add('visible');
+      } else {
+        item.classList.remove('visible');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', checkVisibility);
+  window.addEventListener('resize', checkVisibility);
+
+  // Initial check in case items are already in view
+  checkVisibility();
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const items = document.querySelectorAll('.hero-second-col');
 
   function checkVisibility() {
     const windowHeight = window.innerHeight;
